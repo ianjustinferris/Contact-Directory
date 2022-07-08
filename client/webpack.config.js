6882abf4e9e47f474f10e53ec548ca60d350a544
ianjustinferris <ianjustinferris@gmail.com>
2022-07-08 11:21:34 -0400

    Update client webpack.config.js OUTPUT/PUGINS/MODULES

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,17 +11,42 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
     }
   };
 };
